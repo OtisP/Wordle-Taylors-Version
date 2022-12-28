0aba5c411494b4d015f3a53844d5a21867681a2f
OtisP <otismp52@gmail.com>
2022-12-27 21:55:28 -0500

    add xcode code

---
+++ Wordle Taylors Version/Wordle_Taylors_VersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wordle_Taylors_VersionApp.swift
+//  Wordle Taylors Version
+//
+//  Created by Otis Peterson on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Wordle_Taylors_VersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
