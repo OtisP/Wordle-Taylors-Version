5c99594c711fa05d3b17c363baf10253d15daae7
OtisP <otismp52@gmail.com>
2022-12-29 11:46:40 -0500

    add custom font

--- Wordle Taylors Version/Views/ContentView.swift
+++ Wordle Taylors Version/Views/ContentView.swift
@@ -23,100 +23,122 @@ struct ContentView: View {
         practiceView = PracticeView(practiceViewModel: practiceViewModel)
         self.dailyViewModel = dailyViewModel
         self.practiceViewModel = practiceViewModel
-    
         dailyView.dailyViewModel.getSong(songs: viewModel.songs)
         practiceView.practiceViewModel.getSong(songs: viewModel.songs)
     }
 
     var body: some View {
-        VStack {
-            Text("Wordle (Taylor's Version)")
-                .font(.headline)
-            
-            HStack {
-                Button(action: { self.wordleState = .daily }) {
-                    Text("Daily")
-                        .foregroundColor(self.wordleState == .daily ? .white : .black)
-                        .padding(10)
-                        .background(self.wordleState == .daily ? Color.blue : Color.gray)
-                        .cornerRadius(10)
-                }
-
-                Button(action: { self.wordleState = .practice }) {
-                    Text("Practice")
-                        .foregroundColor(self.wordleState == .practice ? .white : .black)
-                        .padding(10)
-                        .background(self.wordleState == .practice ? Color.blue : Color.gray)
-                        .cornerRadius(10)
-                }
-            }
-
-            switch wordleState {
-            case .daily:
-                // TODO: DRY this
-                if let wonGame = dailyViewModel.wonGameBool {
-                    if wonGame == true {
-                        Text("Winner yay")
-                            .foregroundColor(.green)
-                            .bold()
-                    } else if wonGame == false {
-                        Text("Loser Boo, the right answer was:")
-                            .foregroundColor(.red)
-                            .bold()
-                        Text((dailyViewModel.currentSong?.title ?? "") + " - " + (dailyViewModel.currentSong?.album ?? ""))
-                            .bold()
-                    }
-                }
-                dailyView
-            case .practice:
-                if let wonGame = practiceViewModel.wonGameBool {
-                    if wonGame == true {
-                        Text("Winner yay")
-                            .foregroundColor(.green)
-                            .bold()
-                    } else if wonGame == false {
-                        Text("Loser Boo, the right answer was:")
-                            .foregroundColor(.red)
-                            .bold()
-                        Text((practiceViewModel.currentSong?.title ?? "") + " - " + (practiceViewModel.currentSong?.album ?? ""))
-                            .bold()
-                    }
-                    Button(action: { practiceViewModel.getSong(songs: viewModel.songs) }) {
-                        Text("Start New Game +")
-                    }
-                }
-                practiceView
-            }
-            
-            VStack(alignment: .leading) {
+        ZStack{
+            LinearGradient(
+                gradient: Gradient(colors: [
+                Color(red: 253/255, green: 205/255, blue: 205/255),
+                Color(red: 222/255, green: 192/255, blue: 252/255),
+                Color(red: 206/255, green: 192/255, blue: 252/255),
+                Color(red: 174/255, green: 192/255, blue: 252/255),
+                Color(red: 148/255, green: 225/255, blue: 234/255)
+                ]),
+                startPoint: .topLeading, endPoint: .bottomTrailing
+            )
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Wordle")
+                    .font(.system(size: 28))
+                Text("(Taylor's Version)")
+                    .font(.custom("charlotte", size: 28))
+                    .foregroundColor(Color.black)
                 HStack {
-                    Text("Album")
-                    Picker(selection: $viewModel.selectedAlbum, label: Text("Select an album")) {
-                        ForEach(viewModel.albums, id: \.self) { state in
-                            Text(state)
-                        }
+                    Button(action: { self.wordleState = .daily }) {
+                        Text("Daily")
+                            .foregroundColor(self.wordleState == .daily ? .white : .black)
+                            .padding(10)
+                            .background(self.wordleState == .daily ? Color.blue : Color.gray)
+                            .cornerRadius(10)
                     }
-                }
-                HStack {
-                    Text("Song")
-                    Picker(selection: $viewModel.selectedSong, label: Text("Select a song")) {
-                        ForEach(viewModel.songAndAlbumDict[viewModel.selectedAlbum] ?? [], id: \.self) { city in
-                            Text(city)
-                        }
-                    }
-                }
-            }
 
-            // Button to submit the guess
-            Button(action: {
+                    Button(action: { self.wordleState = .practice }) {
+                        Text("Practice")
+                            .foregroundColor(self.wordleState == .practice ? .white : .black)
+                            .padding(10)
+                            .background(self.wordleState == .practice ? Color.blue : Color.gray)
+                            .cornerRadius(10)
+                    }
+                }
+
                 switch wordleState {
                 case .daily:
-                    dailyViewModel.submitGuess(selectedSong: viewModel.selectedSong, selectedAlbum: viewModel.selectedAlbum)
+                    // TODO: DRY this
+                    if let wonGame = dailyViewModel.wonGameBool {
+                        if wonGame == true {
+                            Text("Winner yay")
+                                .foregroundColor(.green)
+                                .bold()
+                        } else if wonGame == false {
+                            Text("Loser Boo, the right answer was:")
+                                .foregroundColor(.red)
+                                .bold()
+                            Text(
+                                (dailyViewModel.currentSong?.title ?? "")
+                                + " - "
+                                + (dailyViewModel.currentSong?.album ?? "")
+                            )
+                            .bold()
+                        }
+                    }
+                    dailyView
                 case .practice:
-                    practiceViewModel.submitGuess(selectedSong: viewModel.selectedSong, selectedAlbum: viewModel.selectedAlbum)
+                    if let wonGame = practiceViewModel.wonGameBool {
+                        if wonGame == true {
+                            Text("Winner yay")
+                                .foregroundColor(.green)
+                                .bold()
+                        } else if wonGame == false {
+                            Text("Loser Boo, the right answer was:")
+                                .foregroundColor(.red)
+                                .bold()
+                            Text(
+                                (practiceViewModel.currentSong?.title ?? "")
+                                + " - "
+                                + (practiceViewModel.currentSong?.album ?? "")
+                            )
+                            .bold()
+                        }
+                        Button(action: { practiceViewModel.getSong(songs: viewModel.songs) }) {
+                            Text("Start New Game +")
+                        }
+                    }
+                    practiceView
                 }
-            } ) {
-                Text("Submit")
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Album")
+                        Picker(selection: $viewModel.selectedAlbum, label: Text("Select an album")) {
+                            ForEach(viewModel.albums, id: \.self) { state in
+                                Text(state)
+                            }
+                        }
+                    }
+                    HStack {
+                        Text("Song")
+                        Picker(selection: $viewModel.selectedSong, label: Text("Select a song")) {
+                            ForEach(viewModel.songAndAlbumDict[viewModel.selectedAlbum] ?? [], id: \.self) { city in
+                                Text(city)
+                            }
+                        }
+                    }
+                }
+                
+                // Button to submit the guess
+                Button(action: {
+                    switch wordleState {
+                    case .daily:
+                        dailyViewModel.submitGuess(selectedSong: viewModel.selectedSong, selectedAlbum: viewModel.selectedAlbum)
+                    case .practice:
+                        practiceViewModel.submitGuess(selectedSong: viewModel.selectedSong, selectedAlbum: viewModel.selectedAlbum)
+                    }
+                } ) {
+                    Text("Submit")
+                }
             }
         }
     }
